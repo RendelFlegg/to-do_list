5dadea2dfa55d18ad05be2e2244ecc41ede4c550
RendelFlegg <62382457+RendelFlegg@users.noreply.github.com>
2021-11-18 17:36:04 +0500

    Completed Stage 3: Added JS function and CSS rule to mark task as complete (still without styling)

--- To-Do List/task/src/assets/js/main.js
+++ To-Do List/task/src/assets/js/main.js
@@ -6,12 +6,50 @@ function deleteTask(task) {
     return task.parentElement.remove();
 }
 
+function doneTask(task) {
+    return task.parentNode.parentElement.querySelector(".task").classList.toggle("done");
+}
+
 function createTask(taskName) {
     if (taskName !== "") {
         let li = document.createElement("li");
         let label = document.createElement("label");
         let input = document.createElement("input");
         input.type = "checkbox";
+
+
+        input.addEventListener('change', function () {
+            if (this.checked) {
+                doneTask(this)
+                // alert('checked');
+                // input.parentNode.parentElement.querySelector(".task").classList.toggle("done");
+            } else {
+                doneTask(this)
+                // alert('not checked');
+                // input.parentNode.parentElement.querySelector(".task").classList.toggle("done");
+            }
+        })
+
+        // input.addEventListener('change', function () {
+        //     if (this.checked) {
+        //         // alert('checked');
+        //         input.parentNode.parentElement.querySelector(".task").classList.toggle("done");
+        //     } else {
+        //         // alert('not checked');
+        //         input.parentNode.parentElement.querySelector(".task").classList.toggle("done");
+        //     }
+        // })
+
+        // input.addEventListener('change', (event) => {
+        //     if (event.currentTarget.checked) {
+        //         // alert('checked');
+        //         input.parentNode.parentElement.querySelector(".task").classList.toggle("done");
+        //     } else {
+        //         // alert('not checked');
+        //         input.parentNode.parentElement.querySelector(".task").classList.toggle("done");
+        //     }
+        // })
+
         label.appendChild(input);
         let span = document.createElement("span");
         span.className = "task";
